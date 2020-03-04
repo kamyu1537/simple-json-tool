300cb1e38446c5facd2452545a4509912d0fc454
Byeon Seongun <kamyu1537@gmail.com>
2020-03-04 11:11:03 +0900

    :sparkles: index.js 추가

---
+++ index.js
@@ -0,0 +1,46 @@
+const readline = require('readline');
+const fs = require('fs');
+const path = require('path');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.setPrompt('읽을 파일 경로를 입력해 주세요: ');
+
+let originalJSON = '';
+let index = 0;
+let originalObject = '';
+let originalKeys = [];
+let result = {};
+rl.on('line', input => {
+  if (input === '!!!!') {
+    fs.writeFileSync(
+      path.join(process.cwd(), 'output.json'),
+      JSON.stringify(result, null, 2)
+    );
+    process.exit();
+  }
+
+  if (originalJSON === '') {
+    originalJSON = fs.readFileSync(input).toString('utf8');
+    originalObject = JSON.parse(originalJSON);
+    originalKeys = Object.keys(originalObject);
+
+    result = originalObject;
+    rl.setPrompt('> ');
+  } else {
+    // 여기는 등록하는곳
+    const key = originalKeys[index];
+    result[key] = input;
+
+    index++;
+  }
+
+  if (index >= originalKeys.length) {
+    process.exit();
+  }
+
+  rl.prompt();
+});
+
+rl.prompt();
